a833914881297424a32f233b9fdb4e818320e342
andrii.velykorodnii <andrii.velykorodnii@gmail.com>
2023-09-04 11:13:40 +0300

    Add tests for similarity and reduction

---
+++ tests/reduction/test_umap_reduction.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from spaghetti.reduction import UmapReduction
+from spaghetti.errors import UnfitModelError
+
+
+def test_fit_transform():
+    x = np.random.rand(350, 384)
+    reducer = UmapReduction()
+    reduced_data = reducer.fit_transform(x)
+    assert reduced_data.shape[1] == 2
+    assert reduced_data.shape[0] == x.shape[0]
+
+
+def test_transform():
+    x = np.random.rand(350, 384)
+    reducer = UmapReduction()
+    try:
+        reducer.transform(x)
+    except UnfitModelError:
+        reducer.fit(x)
+        reduced_data = reducer.transform(x)
+        assert reduced_data.shape[1] == 2
+        assert reduced_data.shape[0] == x.shape[0]
+        return
+
+    # If it didn't throw the exception, fail the test.
+    assert False
